c0704f62f8e7e5ecd1fe5811e8cf8e447c1aa1d9
ikik1521 <1780398725@qq.com>
2019-05-01 21:38:59 +0800

    登录页面、拦截器

--- src/main/java/cn/sucec/major_adjust_system/interceptor/LoginInterceptor.java
+++ src/main/java/cn/sucec/major_adjust_system/interceptor/LoginInterceptor.java
@@ -12,10 +12,11 @@ public class LoginInterceptor implements HandlerInterceptor{
 	@Override
 	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
 			throws Exception {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method  stub
 		
 	}
 
+	
 	@Override
 	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
 			throws Exception {
